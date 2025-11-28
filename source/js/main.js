14527daf4c29a79fa31a5487d320c293d98c567a
Rodion Yanushkevich <rodion.yanushkevich@mail.ru>
2025-11-28 20:59:14 +0700

    feat: стилизация header init: стилизация hero

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,8 @@
 
-window.addEventListener('DOMContentLoaded', () => {
+import {initMenuToggle} from './modules/burger-menu/burger-menu';
 
+window.addEventListener('DOMContentLoaded', () => {
+  initMenuToggle();
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
